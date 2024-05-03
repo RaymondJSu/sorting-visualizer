40b81b41f9ddcdd5ac8732b4b62a9ea8934ba30e
RaymondJSu <raymond874100@gmail.com>
2024-05-03 16:04:08 -0700

    create sorting algo individually

---
+++ src/SortingAlgorithms/mergeSort.js
@@ -0,0 +1,26 @@
+export const getMergeSort = (array) => {
+    const animations = [];
+    if (array.length <= 1) return array;
+
+  };
+
+  function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ){
+
+  }
+
+  function merge(
+    mainArray,
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ){
+
+  }
