1215ce3046c0ba4d259b41fbe39a80eeb115fa4c
RaymondJSu <raymond874100@gmail.com>
2024-05-10 00:20:04 -0700

    complete bubble sort

---
+++ src/SortingAlgorithms/bubbleSort.js
@@ -0,0 +1,28 @@
+export const getBubbleSort = (array) => {
+    const animations = [];
+    if (array.length <= 1) return array;
+    const auxArr = array.slice();
+    bubbleSort(array, auxArr, animations);
+    return animations;
+  };
+
+  function bubbleSort(arr, auxArr, animations) {
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            animations.push([j, j+1]);
+            animations.push([j, j+1]);
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                animations.push([j, arr[j], j+1, arr[j+1]]);
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+            }
+            else {
+                animations.push([-1, -1, -1, -1]);
+            }
+        }
+    }
+  }
--- src/SortingAlgorithms/quickSort.js
+++ src/SortingAlgorithms/quickSort.js
@@ -1,30 +1,23 @@
-
 export const getQuickSort = (array) => {
-
     const animations = [];
     if (array.length <= 1) return array;
-
-    //const newArray = [...array];
-    //quickSortHelper(newArray, 0, newArray.length - 1, animations);
-    quickSortHelper(array, 0, array.length - 1, animations);
-    
+    const auxArr = array.slice();
+    quickSort(array, 0, array.length - 1, auxArr, animations);
     return animations;
-
 }
 
-
-function quickSortHelper(arr, start, end, animations) {
+function quickSort(arr, start, end, auxArr, animations) {
     if (start >= end) return;
     
     // Partition the array and get the index of the pivot element
-    const pivotIndex = partition(arr, start, end, animations);
+    const pivotIndex = partition(arr, start, end, auxArr, animations);
     
     // Recursively call quickSort on the left and right partitions
-    quickSortHelper(arr, start, pivotIndex - 1, animations);
-    quickSortHelper(arr, pivotIndex + 1, end, animations);
+    quickSort(arr, start, pivotIndex - 1, auxArr, animations);
+    quickSort(arr, pivotIndex + 1, end, auxArr, animations);
 }
 
-function partition(arr, start, end, animations) {
+function partition(arr, start, end, auxArr, animations) {
     const pivot = arr[end];
     let pivotIndex = start;
 
@@ -33,13 +26,18 @@ function partition(arr, start, end, animations) {
         animations.push([i, end]); // Revert comparison highlight
         if (arr[i] < pivot) {
             animations.push([i, arr[pivotIndex]]); // Swap
-            animations.push([pivotIndex, arr[i]]); // Swap
+            
+            // Swap arr[i] and arr[pivotIndex]
             [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
             pivotIndex++;
         }
     }
+    animations.push([pivotIndex, end]);
+    animations.push([pivotIndex, end]);
     animations.push([pivotIndex, arr[end]]); // Swap
-    animations.push([end, arr[pivotIndex]]); // Swap
+    
+
+    // Swap arr[pivotIndex] and arr[end] (move pivot to its correct position)
     [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
 
     return pivotIndex;
