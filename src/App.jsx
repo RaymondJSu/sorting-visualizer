02b66d40cc8c615d3419ca7c506a5561533c4aba
RaymondJSu <raymond874100@gmail.com>
2024-05-10 00:19:28 -0700

    update bubble Sort

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import {getBubbleSort} from './SortingAlgorithms/bubbleSort.js';
 import {getQuickSort} from './SortingAlgorithms/quickSort.js';
 import {getHeapSort} from './SortingAlgorithms/heapSort.js';
 
-const ANIMATION_SPEED_MS = 1;
+const ANIMATION_SPEED_MS = 10;
 
-const NUMBER_OF_ARRAY_BARS = 300;
+const NUMBER_OF_ARRAY_BARS = 30;
 
 const PRIMARY_COLOR = 'turquoise';
 
@@ -60,8 +60,70 @@ export default class SortingVisualizer extends React.Component{
     }
     quickSort() {
         const animations = getQuickSort(this.state.array);
-        
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+              console.log(animations[i], this.state.array[i]);
+              const [barOneIdx, barTwoIdx] = animations[i];
+              // testing
+              const barOneStyle = arrayBars[barOneIdx].style;
+              const barTwoStyle = arrayBars[barTwoIdx].style;
+              const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+              setTimeout(() => {
+                barOneStyle.backgroundColor = color;
+                barTwoStyle.backgroundColor = color;
+              }, i * ANIMATION_SPEED_MS);
+            } else {
+              setTimeout(() => {
+                const [barOneIdx, newHeight] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                barOneStyle.height = `${newHeight}px`;
+              }, i * ANIMATION_SPEED_MS);
+            }
+          }
     }
+
+    bubbleSort() {
+        const animations = getBubbleSort(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('array-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, barOneHeight, barTwoIdx, barTwoHeight] = animations[i];
+              if (barOneHeight !== -1){
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                barOneStyle.height = `${barTwoHeight}px`;
+                barTwoStyle.height = `${barOneHeight}px`;
+              };
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+      }
+
+    testSortingAlgorithms() {
+        for (let i = 0; i < 59; i++) {
+          const array = [];
+          const length = randomIntFromInterval(1, 1000);
+          for (let i = 0; i < length; i++) {
+            array.push(randomIntFromInterval(-1000, 1000));
+          }
+          const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+          const mergeSortedArray = getBubbleSort(array.slice());
+          console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+        }
+      }
     render() {
         const {array} = this.state;
 
@@ -83,7 +145,7 @@ export default class SortingVisualizer extends React.Component{
                     <button className='btn btn-primary' onClick={() => this.heapSort()}>Heap Sort</button>
                     <button className='btn btn-primary' onClick={() => this.bubbleSort()}>Bubble Sort</button>
                     <button className='btn btn-primary' onClick={() => this.testSortingAlgorithms()}>
-                        Test Sorting Algorithms (BROKEN)
+                        Test Sorting Algorithms (change to actual array is js file)
                     </button>
                 </div></>
       )
@@ -95,4 +157,14 @@ export default class SortingVisualizer extends React.Component{
 function randomIntFromInterval(min, max) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function arraysAreEqual(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
   }
